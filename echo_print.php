887a47e1f08d28f5c0bb8d512156add656b5191a
M.R Khodaee <mrkhodaee24@gmail.com>
2024-03-07 09:11:25 -0800

    export echo and print function in php

---
+++ echo_print.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Echo and Print</title>
+</head>
+<body>
+    <?php
+    $name = 'Reza';
+    echo "$name" , 'khodaee';
+    $result = print(2+2);
+    ?>
+</body>
+</html>
