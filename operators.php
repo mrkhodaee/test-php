cedba30597541ff1eccea10097f29ab7d1efca90
M.R Khodaee <mrkhodaee24@gmail.com>
2024-03-07 10:18:36 -0800

    Types of operators

---
+++ operators.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operators</title>
+</head>
+
+<body>
+    <?php
+    $num1 = 10;
+    $num2 = 4;
+    // Total
+    $total = $num1 + $num2;
+    echo "$num1 + $num2 = $total <br>";
+
+    // Minus
+    $minus = $num1 - $num2;
+    echo "$num1 - $num2 = $minus <br>";
+
+    // Beat
+    $beat = $num1 * $num2;
+    echo "$num1 * $num2 = $beat <br>";
+
+    // Split
+    $split = $num1 / $num2;
+    echo "$num1 / $num2 = $split <br>";
+
+    // Can
+    $can = $num1 ** $num2;
+    echo "$num1 ** $num2 = $can";
+
+
+    ?>
+</body>
+
+</html>
